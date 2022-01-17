146485eeafe3ae5ac34ba6dc02b9490802fe16bb
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-01-17 19:08:08 +0530

    Update color_pairnum.h

--- color_pairnum.h
+++ color_pairnum.h
@@ -1,6 +1,6 @@
 #include "common.h"
 
-
+/* function prototype */
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
