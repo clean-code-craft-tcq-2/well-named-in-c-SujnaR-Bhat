82bcb968bac7482f34e09b33ef0589df1c2bb26a
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-01-17 17:24:48 +0530

    Update color_pairnum.c

--- color_pairnum.c
+++ color_pairnum.c
@@ -2,17 +2,6 @@
 #include "color_pairnum.h"
 #include "common.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);    
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
