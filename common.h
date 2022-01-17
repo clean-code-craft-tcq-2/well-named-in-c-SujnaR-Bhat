87c669a4906baa822e7712a91a93d16542b4f006
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-01-17 18:52:18 +0530

    Update common.h

--- common.h
+++ common.h
@@ -5,12 +5,6 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 #define  MAX_COLORPAIR_NAME_CHARS  16
 
 
-
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);    
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
